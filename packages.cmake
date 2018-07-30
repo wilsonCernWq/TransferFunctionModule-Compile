53b92b9d03a8a7f87d598793579a94dd90b2bd1f
WU Qi <wilson.over.cloud@gmail.com>
2018-07-30 14:05:46 -0500

    restruct project

--- packages.cmake
+++ packages.cmake
@@ -8,40 +8,40 @@ if (NOT TARGET OpenGL)
 endif ()
 ## ======================================================================== ##
 if (NOT TARGET glfw)
-  if (EXISTS ${PROJECT_SOURCE_DIR}/glfw)
+  if (EXISTS ${PROJECT_SOURCE_DIR}/external/glfw/upstream/CMakeLists.txt)
     # glfw
     set(GLFW_BUILD_DOCS     OFF)
     set(GLFW_BUILD_EXAMPLES OFF)
     set(GLFW_BUILD_TESTS    OFF)
     set(GLFW_INSTALL        OFF)
-    add_subdirectory(${PROJECT_SOURCE_DIR}/glfw)
+    add_subdirectory(${PROJECT_SOURCE_DIR}/external/glfw/upstream)
     set_target_properties(glfw PROPERTIES
       INTERFACE_COMPILE_DEFINITIONS USE_GLFW=1)
     target_include_directories(glfw INTERFACE
       "$<BUILD_INTERFACE:"
-      "${PROJECT_SOURCE_DIR}/deps;"
-      "${PROJECT_SOURCE_DIR}/glfw;"
-      "${PROJECT_SOURCE_DIR}/glfw/include;"
+      "${PROJECT_SOURCE_DIR}/external/glfw/deps;"
+      "${PROJECT_SOURCE_DIR}/external/glfw/upstream;"
+      "${PROJECT_SOURCE_DIR}/external/glfw/upstream/include;"
       ">")
     # glad
     if (NOT TARGET glad)
       add_library(glad
-        ${PROJECT_SOURCE_DIR}/deps/glad.c)
+        ${PROJECT_SOURCE_DIR}/external/glfw/deps/glad.c)
       target_include_directories(glad PUBLIC
         "$<BUILD_INTERFACE:"
-        "${PROJECT_SOURCE_DIR}/deps;"
-        "${PROJECT_SOURCE_DIR}/glfw;"
-        "${PROJECT_SOURCE_DIR}/glfw/include;"
+        "${PROJECT_SOURCE_DIR}/external/glfw/deps;"
+        "${PROJECT_SOURCE_DIR}/external/glfw/upstream;"
+        "${PROJECT_SOURCE_DIR}/external/glfw/upstream/include;"
         ">")
     endif ()
   else ()
-    messate(FATAL_ERROR "cannot find glfw")
+    message(FATAL_ERROR "cannot find glfw")
   endif ()
 endif ()
 ## ======================================================================== ##
 if (NOT TARGET imgui)
-  if(EXISTS ${PROJECT_SOURCE_DIR}/ImGui)
-    add_subdirectory(${PROJECT_SOURCE_DIR}/ImGui)
+  if (EXISTS ${PROJECT_SOURCE_DIR}/external/imgui/upstream/imgui.h)
+    add_subdirectory(${PROJECT_SOURCE_DIR}/external/imgui)
   else ()
     messate(FATAL_ERROR "cannot find imgui")
   endif ()
